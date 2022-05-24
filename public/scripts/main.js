6a70139086a1c83c0effc7969df6a9728aa3b363
kgytis <kaziukon.gytis@gmail.com>
2022-05-24 22:43:43 +0300

    Dark mode deletion.

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -11,7 +11,30 @@ deleteButtons.forEach(async (btn) => {
 const toggle = document.querySelector('#flexSwitchCheckDefault');
 const darkMode = document.querySelector('#darkModeLabel');
 const body = document.querySelector('body');
-toggle.addEventListener('click', () => {
-    darkMode.textContent = toggle.checked ? 'Dark mode ON' : 'Dark mode OFF'
-    body.classList.toggle('dark-mode');
-})
+const sorting = document.querySelector('.sortingDiv select')
+const cards = document.querySelectorAll('.flexContainer')
+
+
+//Dar reiks patvarkyti
+// toggle.addEventListener('click', () => {
+//     darkMode.textContent = toggle.checked ? 'Dark mode ON' : 'Dark mode OFF'
+//     const bodyTheme = localStorage.getItem('theme1');
+//     const selectTheme = localStorage.getItem('theme2');
+//     const cardsTheme = localStorage.getItem('theme3');
+//     body.classList.toggle(bodyTheme);
+//     sorting.classList.toggle(selectTheme)
+//     cards.forEach((card) => {
+//         card.classList.toggle(cardsTheme)
+//     });
+//     if (!localStorage.getItem('theme1')) {
+//         localStorage.setItem('theme1', 'dark-mode');
+//         localStorage.setItem('theme2', 'select-dark-mode');
+//         localStorage.setItem('theme3', 'card-dark-mode');
+//     }
+//     else {
+//         localStorage.removeItem('theme1')
+//         localStorage.removeItem('theme2')
+//         localStorage.removeItem('theme3')
+//     }
+// })
+

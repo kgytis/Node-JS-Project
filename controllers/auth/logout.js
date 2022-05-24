b2fb23ca5ac9d6c76fa3bec731a5bd4c0c594583
kgytis <kaziukon.gytis@gmail.com>
2022-05-24 13:29:19 +0300

    Logout. Redirection added

--- controllers/auth/logout.js
+++ controllers/auth/logout.js
@@ -1,5 +1,11 @@
 const logoutUser = async (req, res) => {
-    return res.clearCookie('accessToken').status(200).send({ msg: "You have been logged out successfully." })
+    try {
+        //return res.clearCookie('accessToken').status(200).send({ msg: "You have been logged out successfully." })
+        return res.clearCookie('accessToken').status(200).redirect('/')
+    } catch (err) {
+        res.send({ err: err })
+    }
+
 };
 
 export default logoutUser

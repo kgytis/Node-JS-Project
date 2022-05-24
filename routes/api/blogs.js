8702963c523b8f6aedc3785af034357a7fe6512d
kgytis <kaziukon.gytis@gmail.com>
2022-05-24 08:57:23 +0300

    Primary API routes setup (users+blogs)

---
+++ routes/api/blogs.js
@@ -0,0 +1,12 @@
+import express from 'express';
+
+//Controllers imports
+
+//Middleware imports
+
+const apiBlogsRouter = express.Router();
+
+//apiBlogsRouter.get('/blog', allBlogs);
+//apiBlogsRouter.get('/blog/:id', blogByID);
+
+export default apiBlogsRouter
---
+++ routes/api/users.js
@@ -0,0 +1,12 @@
+import express from 'express';
+
+//Controllers imports
+
+//Middleware imports
+
+const apiUsersRouter = express.Router();
+
+//apiUsersRouter.get('/users', allUsers);
+//apiUsersRouter.get('/users/:id', userByID);
+
+export default apiUsersRouter
